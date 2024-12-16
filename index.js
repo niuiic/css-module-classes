676e33c1a05bd893f30c280b83aa831faab872e4
niuiic <niuiicde@gmail.com>
2024-12-16 16:35:41 +0800

    feat: improve performance

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ export const classes = (...args) =>
   args
     .map((x) => (Array.isArray(x) ? getClassFromModule(x[0], x[1]) : x))
     .filter(Boolean)
-    .flat()
     .join(' ')
 
 const getClassFromModule = (module, className) => {
   if (!module[className]) {
     throw new Error(`class name ${className} does not exist`)
   }
-  return [className, module[className]]
+  return [className, module[className]].join(' ')
 }
